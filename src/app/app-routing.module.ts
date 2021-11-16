3112c6fc5ff3b6354526329ffcdbe5d09baca939
Shalda <shalda.dmitry@gmail.com>
2021-11-16 17:07:43 +0200

    src fix for stackb v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInComponent } from 'src/app/auth/sign-in/sign-in.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard'
